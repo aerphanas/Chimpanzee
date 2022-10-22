35d5dd7855f31e3c18cbe1ae57cd34560add7c51
aerphanas <muhamadaviv14@gmail.com>
2022-10-22 09:36:22 +0000

    cleaning some code

--- src/Caesar.hs
+++ src/Caesar.hs
@@ -3,10 +3,10 @@ module Caesar (encrypt,decrypt) where
 import qualified Data.Char as DC -- chr, ord, isAlpha, isUpper
 
 encrypt::Int -> [Char] -> [Char]
-encrypt x = map (DC.chr . doEncAlg x . DC.ord)
+encrypt shift = map (DC.chr . doEncAlg shift . DC.ord)
 
-decrypt :: Int -> [Char] -> [Char]
-decrypt x = map (DC.chr . doDecAlg x . DC.ord)
+decrypt::Int -> [Char] -> [Char]
+decrypt shift = map (DC.chr . doDecAlg shift . DC.ord)
 
 -- this function will check if the input is Integer of Char
 -- if the input is not char this function wil return its value
@@ -16,13 +16,15 @@ doEncAlg::Int -> Int -> Int
 doEncAlg shift x
     | DC.isAlpha . DC.chr $ x = if checkOver then (x - 25) +  (shift - 1) else x + shift
     | otherwise               = x -- do algorithm only on alphabetic
-    where checkAlpha = if DC.isUpper . DC.chr $ x then 90 else 122
-          checkOver  = (x + shift) > checkAlpha
+    where 
+        checkAlpha = if DC.isUpper . DC.chr $ x then 90 else 122
+        checkOver  = (x + shift) > checkAlpha
 
 -- this function works like doEncAlg, but do the opposite
 doDecAlg::Int -> Int -> Int
 doDecAlg shift x
     | DC.isAlpha . DC.chr $ x = if checkOver then (x + 25) -  (shift + 1) + 2 else x - shift
     | otherwise               = x -- do algorithm only on alphabetic
-    where checkAlpha = if DC.isUpper . DC.chr $ x then 65 else 97
-          checkOver  = (x - shift) < checkAlpha
+    where
+        checkAlpha = if DC.isUpper . DC.chr $ x then 65 else 97
+        checkOver  = (x - shift) < checkAlpha
--- src/Rot13.hs
+++ src/Rot13.hs
@@ -5,12 +5,12 @@ import qualified Data.Char as DC -- chr, ord, isAlpha, isUpper
 encrypt::[Char] -> [Char]
 encrypt = map (DC.chr . doEncAlg . DC.ord) -- convert Alphabet, do the encription algorithm and give the result
 
-decrypt :: [Char] -> [Char]
+decrypt::[Char] -> [Char]
 decrypt = map (DC.chr . doDecAlg . DC.ord) -- do the decryption algorithm
 
 -- checking if alphabet is greater than number and do the calculation
 -- if the number greater than z or 90 in uppercase and 122 in lowercase
--- the algorithm wilbe (number - 25) - (13 - 1) or else x + 13
+-- the algorithm will be (number - 25) - (13 - 1) or else x + 13
 doEncAlg::Int -> Int
 doEncAlg x
     | DC.isAlpha . DC.chr $ x = if checkOver then (x - 25) +  (13 - 1) else x + 13
@@ -20,7 +20,7 @@ doEncAlg x
 
 -- the calculation is same as encryption but this time we do the opposite
 -- if the number smaller than 'a' (97) and 'A' (65) letter, and do opposite calculation
--- the algorithm wilbe (number + 25) - (13 + 1) or else x - 13
+-- the algorithm will be (number + 25) - (13 + 1) or else x - 13
 doDecAlg::Int -> Int
 doDecAlg x
     | DC.isAlpha . DC.chr $ x = if checkOver then (x + 25) -  (13 + 1) + 2 else x - 13
